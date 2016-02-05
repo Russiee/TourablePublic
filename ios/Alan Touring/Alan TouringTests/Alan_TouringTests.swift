583748333cde29de016a05481e7b312fa3876f18
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-02-05 17:32:25 +0000

    IOS-1 Initiate XCode project

---
+++ ios/Alan Touring/Alan TouringTests/Alan_TouringTests.swift
@@ -0,0 +1,36 @@
+//
+//  Alan_TouringTests.swift
+//  Alan TouringTests
+//
+//  Created by Daniel Baryshnikov on 05/02/2016.
+//  Copyright © 2016 Hobbyte. All rights reserved.
+//
+
+import XCTest
+@testable import Alan_Touring
+
+class Alan_TouringTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
