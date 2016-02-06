2e34663b4ac192541b708851a2655c01b665f06f
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2016-02-06 19:13:12 +0000

    structure for displaying previously laoded tours

--- android/app/src/main/java/com/hobbyte/touringandroid/StartActivity.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/StartActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartActivity extends Activity {
@@ -15,6 +17,16 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        loadPreviousTours();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // needed in the event that a user adds a tour and then returns to this screen
+        loadPreviousTours();
     }
 
     /**
@@ -39,5 +51,18 @@ public class StartActivity extends Activity {
             toast.show();
             textKey.setText("");
         }
+    }
+
+    /**
+     * If the user has tours saved to the device, show their names and expiry information.
+     */
+    private void loadPreviousTours() {
+        // TODO: need to finalise design
+        // TODO: hook up to filesystem to check if tours exist
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.previousToursLayout);
+        View noToursText = getLayoutInflater().inflate(R.layout.text_no_tours, layout, false);
+
+        layout.addView(noToursText);
     }
 }
