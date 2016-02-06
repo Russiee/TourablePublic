fd7499d331952a70e7386d72cadf12b520f3919e
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2016-02-06 18:27:14 +0000

    made a method stub to check if the provided tour key is valid

--- android/app/src/main/java/com/hobbyte/touringandroid/StartActivity.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/StartActivity.java
@@ -2,12 +2,42 @@ package com.hobbyte.touringandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+    public static final String TAG = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    /**
+     * Checks if the provided tour key is valid. If so, continue to the next activity, otherwise
+     * inform the user that the key was invalid.
+     * @param v the submit button
+     */
+    public void checkTourKey(View v) {
+        // TODO: hook this up to the api to securely validate the key
+
+        EditText textKey = (EditText) findViewById(R.id.textEnterTour);
+        String tourKey = textKey.getText().toString();
+
+        // this will be changed in the future
+        if (tourKey.equals("jeroenTour")) {
+            Log.d(TAG, "Valid key");
+            // move to next activity
+        } else {
+            Log.d(TAG, "Invalid key");
+            Toast toast = Toast.makeText(this, "Invalid tour key", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 20);
+            toast.show();
+            textKey.setText("");
+        }
     }
 }
