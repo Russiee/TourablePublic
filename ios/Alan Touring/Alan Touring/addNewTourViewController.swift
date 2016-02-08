2f3c4e6100ea763b1f60b47861d804fbbd3459ac
Alex Gubbay <alexander.gubbay@kcl.ac.uk>
2016-02-08 13:20:22 +0000

    added ViewController for the addTourView. No transitions yet

---
+++ ios/Alan Touring/Alan Touring/addNewTourViewController.swift
@@ -0,0 +1,13 @@
+//
+//  addNewTourViewController.swift
+//  Alan Touring
+//
+//  Created by Alex Gubbay on 08/02/2016.
+//  Copyright © 2016 Hobbyte. All rights reserved.
+//
+
+import UIKit
+
+class addNewTourViewController: UIViewController {
+
+}
