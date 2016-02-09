1cdb99935f167acd1945528ac68490865e2663a1
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2016-02-09 06:50:50 +0000

    AND-11 made classes for DB management that need to be fleshed out

---
+++ android/app/src/main/java/com/hobbyte/touringandroid/helpers/TourDBManager.java
@@ -0,0 +1,15 @@
+package com.hobbyte.touringandroid.helpers;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Performs all required database operations.
+ */
+//public class TourDBManager extends SQLiteOpenHelper {
+public class TourDBManager {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "TourData.db";
+
+
+}
