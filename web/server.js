1be6769361d802ee78ad0e6d52c60c0f9fd20e43
philefstat <phil.efstat@gmail.com>
2016-02-12 03:19:26 +0000

    WEB-6 set up angular

--- web/server.js
+++ web/server.js
@@ -3,11 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/app/index.html');
+	res.sendFile(__dirname + '/client/index.html');
 });
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-    console.log("We're live on port " + port);
+	console.log("We're live on port " + port);
 });
