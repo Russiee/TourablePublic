a755150e9f028c3c3765c95d0c86ad1706e3380f
philefstat <phil.efstat@gmail.com>
2016-02-12 13:01:56 +0000

    WEB-6 express serve static files properly

---
+++ web/client/app/app.module.js
@@ -0,0 +1,1 @@
+var touring = angular.module('touring', []);
--- web/server.js
+++ web/server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
-app.get('*', function(req, res) {
-	res.sendFile(__dirname + '/client/index.html');
-});
+app.use(express.static(__dirname + '/client'));
+
+app.route('*')
+	.get(function(req, res) {
+		res.sendFile(path.resolve('client/index.html'));
+	});
+
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 
