605719c880ea397ba22c01a974db25fd05f841ad
philefstat <phil.efstat@gmail.com>
2016-02-12 15:45:21 +0000

    WEB-10 setup jshint

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon')
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-    nodemon({
-        script: 'server.js',
-        ext: 'js html', 
-        env: { 'NODE_ENV': 'development' }
-      })
+gulp.task('default', ['lint'], function() {
+	nodemon({
+		script: 'server.js',
+		ext: 'js html',
+		env: { 'NODE_ENV': 'development' }
+	});
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish'));
 });
