e11c4c1a12e02bc28aa9490f42340e7634c89b2d
FedericoKCL <fed.deployment@aol.co.uk>
2016-02-13 13:34:24 +0000

    IOS-Refactoring tidied up tourIdParser

--- ios/Alan Touring/Alan Touring/tourIdParser.swift
+++ ios/Alan Touring/Alan Touring/tourIdParser.swift
@@ -11,48 +11,35 @@ import Foundation
 let TableUpdateNotificationKey = "tableAddWasComplete"
 
 public class tourIdParser {
-    
-    //READ ONLY. TO ADD ITEM USE updateArray()
 
     var API = ApiConnector.init()
-
-    
     
     
     public init(){
-        
         //pulls the latest version from the cache.
         if(NSUserDefaults.standardUserDefaults().stringArrayForKey("Array")==nil){
-             let newArray = [AnyObject]()
+            let newArray = [AnyObject]()
             self.saveArray(newArray)
         }
-        
-        
-            }
+    }
     
   
     //Clears all records from the array permanently. Use with caution
     public func clearArray(){
         let newArray = [AnyObject]()
         saveArray(newArray)
-        
     }
     
    public func deleteTourIdAtRow(row:Int){
-    
-    var newArray : [AnyObject] = NSUserDefaults.standardUserDefaults().objectForKey("Array")! as AnyObject as! [AnyObject]
-    NSUserDefaults.standardUserDefaults().removeObjectForKey(newArray[row] as! String)
-       newArray.removeAtIndex(row)
+        var newArray : [AnyObject] = NSUserDefaults.standardUserDefaults().objectForKey("Array")! as AnyObject as! [AnyObject]
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(newArray[row] as! String)
+        newArray.removeAtIndex(row)
         saveArray(newArray)
-        
     }
    
     
     //Adds a new tourId to the array
-
-
     public func updateArray(tourId: String){
-
         //Duplicates the array, creating a mutable version that the new tourId can be added to.
         var newArray : [AnyObject] = NSUserDefaults.standardUserDefaults().objectForKey("Array") as! NSMutableArray as [AnyObject]
         newArray.append(tourId)
@@ -65,9 +52,6 @@ public class tourIdParser {
     //saves the copy of the array passed to it persistently and updates working copy kept in this class.
     //Never called outside of this class
     private func saveArray(newArray: AnyObject){
-        
-        //This updates the working copy
-
         //Stores the Array in NSUserDefaults, overwriting existing copy
         NSUserDefaults.standardUserDefaults().setObject(newArray, forKey: "Array")
         //Commits changes to memory, required for iOS 7 and below.
@@ -84,7 +68,6 @@ public class tourIdParser {
         var dict = metadata.dictionaryWithValuesForKeys(keys)
         let tourCode = dict["code"]!
 
-
         let fuckEverything = dict as NSDictionary
         NSUserDefaults.standardUserDefaults().setObject(fuckEverything, forKey: tourCode[0] as! String)
         NSUserDefaults.standardUserDefaults().synchronize()
@@ -95,27 +78,21 @@ public class tourIdParser {
             self,
             selector: "TableChanged:",
             name: "TabledDataChanged",
-            object: nil)
-
-   
+            object: nil
+        )
     }
     
     func getTourMetadata(tourCode: String) -> Dictionary<String,AnyObject>{
        return NSUserDefaults.standardUserDefaults().objectForKey(tourCode) as! Dictionary<String,AnyObject>
     }
     
-    
     func notify() {
         NSNotificationCenter.defaultCenter().postNotificationName(TableUpdateNotificationKey, object: self)
         print("notify called")
     }
     
-    
-      //temporary method for getting tourIds that have been added for checking the table updates.
+    //temporary method for getting tourIds that have been added for checking the table updates.
     public func getAllTours() -> NSMutableArray {
-
         return NSUserDefaults.standardUserDefaults().objectForKey("Array") as! NSMutableArray
     }
-    
-
 }
