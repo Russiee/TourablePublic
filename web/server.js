08d02ca19736274249477a3e4aa2426e1b0270f7
philefstat <phil.efstat@gmail.com>
2016-02-13 01:09:10 +0000

    WEB-19 add basic auth

--- web/server.js
+++ web/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var wwwhisper = require('connect-wwwhisper');
+
+app.use(wwwhisper());
 
 app.use(express.static(__dirname + '/client'));
 
@@ -13,6 +16,17 @@ app.route('*')
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
+
+var credentials = auth({ name: 'something', pass: 'whatever' })
+
+  if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    res.end('Access granted')
+  }
+
 
 app.listen(port, function () {
 	console.log("We're live on port " + port);
