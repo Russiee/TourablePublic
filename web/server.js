5474b0eedbd07226bdb32a45b0d320a2aca0b0bc
philefstat <phil.efstat@gmail.com>
2016-02-13 01:10:52 +0000

    WEB-19 add basic auth remove old code

--- web/server.js
+++ web/server.js
@@ -17,17 +17,6 @@ app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 
-var credentials = auth({ name: 'something', pass: 'whatever' })
-
-  if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
-    res.statusCode = 401
-    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-    res.end('Access denied')
-  } else {
-    res.end('Access granted')
-  }
-
-
 app.listen(port, function () {
 	console.log("We're live on port " + port);
 });
