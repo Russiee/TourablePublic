c160e85726b832f28677d1e34e7e1617b06dc604
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-02-13 12:11:15 +0000

    IOS-Refactoring refactored API Connector class to add readability

--- ios/Alan Touring/Alan Touring/ApiConnector.swift
+++ ios/Alan Touring/Alan Touring/ApiConnector.swift
@@ -14,18 +14,21 @@ class ApiConnector: NSObject, NSURLConnectionDelegate{
     var urlPath: String = ""
     
     func startConnection(tourId: String){
-        urlPath = ""
-        let newData = NSMutableData()
-        data = newData
+        let resetData = NSMutableData()
+        //Reseting data to blank with every new connection
+        data = resetData
+        //The path to where the Tour Data is stored
         urlPath = "https://touring-api.herokuapp.com/api/v1/key/verify/" + tourId
+        //Standard URLConnection method
         let url: NSURL = NSURL(string: urlPath)!
         let request: NSURLRequest = NSURLRequest(URL: url)
-        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)!
         connection.start()
     }
     
     
     func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        //Storing the data for use
         self.data.appendData(data)
     }
     
@@ -38,15 +41,17 @@ class ApiConnector: NSObject, NSURLConnectionDelegate{
         do {
             let jsonResult: NSArray = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSArray
 
-            self.dealWithData(jsonResult)
+            self.storeJson(jsonResult)
         }
         catch let err as NSError{
+            //Need to let user know if the tourID they entered was faulty here
             print(err.description)
         }
     
     }
     
-    func dealWithData(JSONData: NSArray){
+    func storeJson(JSONData: NSArray){
+        //Storing Meta Data so we can access it for other use
         let tour = tourIdParser.init()
         tour.addTourMetaData(JSONData)
     }
