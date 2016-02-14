9b0f667ffc77063ca0806cfee458a73b1e3515b9
philefstat <phil.efstat@gmail.com>
2016-02-14 16:34:10 +0000

    WEB-21 add key verification service

--- web/client/app/components/home/home.controller.js
+++ web/client/app/components/home/home.controller.js
@@ -1,9 +1,14 @@
-'use strict';
-
 angular.module('touring')
-	.controller('HomeCtrl', function ($scope, $location) {
+	.controller('HomeCtrl', function ($scope, $location, keyFactory) {
+    
         $scope.startTour = function (key) {
-            $location.url('/tour?key=' + key);
+            var promise = keyFactory.verify(key);
+            promise.then(function(response) {
+                $location.url('/tour?key=' + key)
+                console.log('Success: ' + response);
+            }, function(error) {
+                alert("KEY REJECTED ASSHOLE")
+                console.log('Failed: ' + error);
+            });
         }
-
 	});
--- web/client/app/components/tour/tour.controller.js
+++ web/client/app/components/tour/tour.controller.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('touring')
 	.controller('TourCtrl', function ($scope, $location) {
         $scope.location = $location.search().key;
