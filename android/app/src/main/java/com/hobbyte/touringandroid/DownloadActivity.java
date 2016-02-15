396786773af591b7b5e60d164a6e78a99ff35386
nathanojb <jd_burton@hotmail.co.uk>
2016-02-15 17:22:56 +0000

    AND-13-Added loading circle

--- android/app/src/main/java/com/hobbyte/touringandroid/DownloadActivity.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/DownloadActivity.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class DownloadActivity extends Activity {
+
+    private ProgressBar progressBar;
+    private TextView bottomTextView;
+    private Button downloadImagesButton;
+    private Button downloadVideoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,20 +21,23 @@ public class DownloadActivity extends Activity {
 
         setDownloadSizeLabels();
 
-        Button downloadImagesButton = (Button) findViewById(R.id.imageOptionButton);
-        Button downloadVideoButton = (Button) findViewById(R.id.videoOptionButton);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        bottomTextView = (TextView) findViewById(R.id.bottomText);
 
+        downloadImagesButton = (Button) findViewById(R.id.imageOptionButton);
+        downloadVideoButton = (Button) findViewById(R.id.videoOptionButton);
         downloadImagesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO api call for images here
+                animateStuff("");
             }
         });
-
         downloadVideoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO api call for video here
+                animateStuff(" & video");
             }
         });
     }
@@ -37,6 +46,16 @@ public class DownloadActivity extends Activity {
 
         TextView imageDownloadSizeLabel = (TextView) findViewById(R.id.imageDownloadSizeLabel);
         TextView videoDownloadSizeLabel = (TextView) findViewById(R.id.videoDownloadSizeLabel);
+
+        imageDownloadSizeLabel.setText("100GB");
+        videoDownloadSizeLabel.setText("10Kb");
+    }
+
+    private void animateStuff(String stuff) {
+        downloadImagesButton.setEnabled(false);
+        downloadVideoButton.setEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
+        bottomTextView.setText("Downloading images" + stuff + "...");
     }
 
 }
