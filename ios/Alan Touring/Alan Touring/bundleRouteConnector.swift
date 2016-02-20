e4f459139ea4cc085e1fb4f8617065ac053d9ad5
FedericoKCL <fed.deployment@aol.co.uk>
2016-02-20 17:04:21 +0000

    IOS-20 resolved API connection issue

--- ios/Alan Touring/Alan Touring/bundleRouteConnector.swift
+++ ios/Alan Touring/Alan Touring/bundleRouteConnector.swift
@@ -4,27 +4,47 @@ import UIKit
 //Handles API connectivity for tour sections. As these are handled differently to
 //tour sections
 class bundleRouteConnector: NSObject, NSURLConnectionDelegate{
-    
+
     lazy var data = NSMutableData()
     var urlPath: String = ""
-    
+
     //Makes the connection to the API
     private func startConnection( objectID: String){
-        
-        
+
+
         let resetData = NSMutableData()
         //Reseting data to blank with every new connection
         data = resetData
 
         //The path to where the Tour Data is stored
-       
+
         urlPath = "https://touring-api.herokuapp.com/api/v1/section/"+objectID
         //Standard URLConnection method
-        let request: NSURLRequest = NSURLRequest(URL: NSURL(string: urlPath)!)
-        
-        //change to URLSession
-        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
-        connection.start()
+        //        let request: NSURLRequest = NSURLRequest(URL: NSURL(string: urlPath)!)
+        //
+        //        //change to URLSession
+        //        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        //        connection.start()
+        let request = NSURLRequest(URL: NSURL(string: urlPath)!)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+
+        let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            self.data.appendData(data!)
+            do {
+                let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+
+                self.storeTourJSON(jsonResult)
+            }
+            catch let err as NSError{
+                //Need to let user know if the tourID they entered was faulty here
+                print(err.description)
+
+            }
+        }
+        task.resume()
+
+
     }
     
     
@@ -37,34 +57,35 @@ class bundleRouteConnector: NSObject, NSURLConnectionDelegate{
         startConnection(objectId)
     }
     
+    //KEEP FOR NOW (SHOULD HANDLE 'SAVING TOUR' AFTER DOWNLOAD). KEEP IN CASE CONNECTION BREAKS AGAIN
     //handles data after connection is complete.
-    private func connectionDidFinishLoading(connection: NSURLConnection!) {
-        
-        do {
-            
-            let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
-            
-            self.storeTourJSON(jsonResult)
-        }
-        catch let err as NSError{
-            //Need to let user know if the tourID they entered was faulty here
-           
-            print(err.description)
-            
-
-        }
-        
-    }
-
+    //    private func connectionDidFinishLoading(connection: NSURLConnection!) {
+    //
+    //        do {
+    //
+    //            let jsonResult: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+    //
+    //            self.storeTourJSON(jsonResult)
+    //        }
+    //        catch let err as NSError{
+    //            //Need to let user know if the tourID they entered was faulty here
+    //
+    //            print(err.description)
+    //
+    //
+    //        }
+    
+    //    }
+    
     //Takes the metadata and passes it to the tourIdParser.
     func storeTourJSON(JSONData: NSDictionary){
         //Storing Meta Data so we can access it for other use
         
         tourDataParser.init().saveTourSection(JSONData)
-       //print(JSONData)
+        //print(JSONData)
         
     }
-   
-   
+    
+    
     
 }
