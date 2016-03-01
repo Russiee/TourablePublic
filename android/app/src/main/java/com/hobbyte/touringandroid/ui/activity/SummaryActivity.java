b0a1e9b5e0fd22e451925321ce8c313bb25869c8
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2016-03-01 23:41:55 +0000

    AND-31 Made local DB operations a lot easier. No need to worry about getting readable/writable instances anymore. It's all handled in TourDBManager. The only thing that you must do is call close() in onPause() or onStop() of an Activity (see javadoc)

--- android/app/src/main/java/com/hobbyte/touringandroid/ui/activity/SummaryActivity.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/ui/activity/SummaryActivity.java
@@ -1,6 +1,5 @@
 package com.hobbyte.touringandroid.ui.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +8,8 @@ import android.widget.TextView;
 
 import com.hobbyte.touringandroid.R;
 import com.hobbyte.touringandroid.io.FileManager;
-import com.hobbyte.touringandroid.tourdata.Tour;
-import com.hobbyte.touringandroid.tourdata.TourBuilder;
 
 import org.json.JSONObject;
-
-import java.util.regex.Pattern;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -24,8 +19,6 @@ public class SummaryActivity extends AppCompatActivity {
     private String keyID;
     private String title;
 
-    private Pattern p;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +26,6 @@ public class SummaryActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         keyID = intent.getStringExtra(KEY_ID);
-        p = Pattern.compile("https?:\\/\\/[\\w\\.\\/]*\\/(\\w*\\.(jpe?g|png))");
-
-        /*TourBuilder tb = new TourBuilder(keyID, this);
-        tb.start();*/
 
         loadTourDescription();
     }
