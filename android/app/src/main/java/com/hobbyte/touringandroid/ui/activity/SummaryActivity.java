3b8ed2bf5e545da4be6a57d279f020c88ee34518
Max Karasinski <maximilian.karasinski@kcl.ac.uk>
2016-03-04 18:46:31 +0000

    AND-18 Tweaked the SummaryActivity layout for the open/update tour buttons. Set up scaffolding in SummaryActivity to react to an update being available

--- android/app/src/main/java/com/hobbyte/touringandroid/ui/activity/SummaryActivity.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/ui/activity/SummaryActivity.java
@@ -1,9 +1,14 @@
 package com.hobbyte.touringandroid.ui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.opengl.Visibility;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,10 +17,15 @@ import com.hobbyte.touringandroid.io.FileManager;
 
 import org.json.JSONObject;
 
+import java.util.Set;
+
 public class SummaryActivity extends AppCompatActivity {
 
     private static final String TAG = "SummaryActivity";
     public static final String KEY_ID = "keyID";
+
+    private Button openButton;
+    private Button updateButton;
 
     private String keyID;
     private String title;
@@ -27,6 +37,17 @@ public class SummaryActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         keyID = intent.getStringExtra(KEY_ID);
+
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences(
+                getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE);
+        Set<String> toUpdate = prefs.getStringSet(getString(R.string.prefs_tours_to_update), null);
+
+//        if (true) {
+        if (toUpdate != null && toUpdate.contains(keyID)) {
+            updateButton = (Button) findViewById(R.id.buttonUpdateTour);
+            updateButton.setVisibility(View.VISIBLE);
+        }
 
         loadTourDescription();
     }
@@ -55,10 +76,24 @@ public class SummaryActivity extends AppCompatActivity {
         }
     }
 
+    public void doTourUpdate(View v) {
+        openButton.setEnabled(false);
+        updateButton.setEnabled(false);
+
+        new UpdateTask().execute();
+    }
 
     @Override
     public void onBackPressed() {
             Intent intent = new Intent(this, StartActivity.class);
             startActivity(intent);
+    }
+
+    private class UpdateTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            return null;
+        }
     }
 }
