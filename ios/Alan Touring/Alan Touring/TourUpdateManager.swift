b04300cd0c5fa9c1e31546fbd6060c16a67a33c8
FedericoKCL <fed.deployment@aol.co.uk>
2016-03-05 19:33:53 +0000

    IOS-43 start offf with a todo list

---
+++ ios/Alan Touring/Alan Touring/TourUpdateManager.swift
@@ -0,0 +1,20 @@
+//
+//  TourUpdateManager.swift
+//  Alan Touring
+//
+//  Created by Federico on 05/03/2016.
+//  Copyright © 2016 Hobbyte. All rights reserved.
+//
+
+import Foundation
+
+public class TourUpdateManager {
+
+    init() { }
+
+    // TODO1: on create get a specific tourID to be managed in a local variable
+
+    // TODO2: add functionality to connect to the API (through APIConnector) and retrieve the 'last-updated' field.
+    // Check against saved tourMetaData (throught tourIDParser). Re-downlaoad if needed.
+
+}
