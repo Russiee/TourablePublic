873fbc6751cb63007bd41370ccb3bd8752bc3cbe
ozliftoff <gusgub@hotmail.co.uk>
2016-03-06 12:27:56 +0000

    IOS-44 internet connection check function added. Will not allow user to enter tour with out connection

--- ios/Alan Touring/Alan Touring/ApiConnector.swift
+++ ios/Alan Touring/Alan Touring/ApiConnector.swift
@@ -5,9 +5,11 @@
 //  Created by Alex Gubbay on 12/02/2016.
 //  Copyright © 2016 Hobbyte. All rights reserved.
 //
-//
+// internet connectivity code from: http://stackoverflow.com/questions/25623272/how-to-use-scnetworkreachability-in-swift/25623647#25623647
+//See isConnectedToNetwork for full credit.
 import Foundation
 import UIKit
+import SystemConfiguration
 
 //keys for notifying observers of outcome of the key verification route
 let invalidIdNotificationKey = "InvalidKeyEnteredNotification"
@@ -107,5 +109,40 @@ class ApiConnector: NSObject, NSURLConnectionDelegate{
         tourIdForSummary = trimmedTourId
         return trimmedTourId
     }
+    
+    //checks if the device currently has an active internet connection.
+    func isConnectedToNetwork() -> Bool {
+        //Code and comment guidance from http://stackoverflow.com/questions/25623272/how-to-use-scnetworkreachability-in-swift/25623647#25623647
+        //With thanks to user: Martin R http://stackoverflow.com/users/1187415/martin-r
+        //Creates the socket address structure.
+        var zeroAddress = sockaddr_in()
+        //Gives the size of this structure, converted to UInt8
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        //Converts AF_INET to correct format for sin_family
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        //passes the address of the structure to the closure where it is used as an argument
+        //In the SCNetworkReachabilityCreateWthAddress()
+        //The guard let assigns the unwrapped value to the defaultRouteReachability if it is not nill.
+        //If it is nill, no internet connection, return false.
+        guard let defaultRouteReachability = withUnsafePointer(&zeroAddress, {
+            //This conversion is required because the function expects a pointer to sockaddr not sockaddr_in
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }) else {
+            return false
+        }
+        //Returns a managed object.
+        var flags : SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            //If there are no flags, no connection, return false.
+            return false
+        }
+        //Checks for flags indicating connectiivty status.
+        let isReachable = flags.contains(.Reachable)
+        let needsConnection = flags.contains(.ConnectionRequired)
+        //Only got a working connection if the system is connected and it is not waiting for a connection.
+        return (isReachable && !needsConnection)
+    }
+    
     
 }
