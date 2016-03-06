0a87a7b0ab13d07d518e7ef18b47a868a5d041f8
FedericoKCL <fed.deployment@aol.co.uk>
2016-03-06 13:02:46 +0000

    IOS-43 comparing dates and downloading update straight away if there is any

--- ios/Alan Touring/Alan Touring/TourUpdateManager.swift
+++ ios/Alan Touring/Alan Touring/TourUpdateManager.swift
@@ -12,21 +12,49 @@ public class TourUpdateManager {
 
     let tourCode: String!
     var currentMetadata: Dictionary<String,AnyObject>!
+    var newMetadata: NSDictionary!
 
     init(tourCodetoCheck: String) {
         self.tourCode = tourCodetoCheck
         self.currentMetadata = TourIdParser().getTourMetadata(tourCode)
-        compareOldAndNewMetadata()
+        checkForUpdates()
     }
 
-    func compareOldAndNewMetadata() {
+    func downloadNewMetadata() {
         let connection = ApiConnector.init()
         connection.initateConnection(tourCode, isUpdate: true)
-        let newMetadata = connection.getTourMetadata(tourCode)
+        newMetadata = connection.getTourMetadata(tourCode)
+
         print(newMetadata)
+
     }
 
-    // TODO2: add functionality to connect to the API (through APIConnector) and retrieve the 'last-updated' field.
-    // Check against saved tourMetaData (throught tourIDParser). Re-downlaoad if needed.
+    func checkForUpdates() {
+        downloadNewMetadata()
 
+        let currentDateString = currentMetadata["updatedAt"]
+        let newDateString = newMetadata["updatedAt"]
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSSz"
+        let currentDate = dateFormatter.dateFromString(currentDateString as! String)
+        let newDate = dateFormatter.dateFromString(newDateString as! String)
+
+        print(currentDateString)
+        print(newDateString)
+
+        print(currentDate)
+        print(newDate)
+
+        let dateComparison = currentDate!.compare(newDate!)
+
+        switch (dateComparison) {
+            case NSComparisonResult.OrderedDescending:
+                _ = TourIdParser().getTourMetadata(tourCode)
+                // here you should 'say' that the app is updating stuff
+            default:
+                // here trigger alert that everything is up to date
+                print("")
+        }
+    }
 }
