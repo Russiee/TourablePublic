3afda452e1484a77d9ea3028bd71d4fec31e6a82
pete2269 <peter_barta_3@hotmail.com>
2016-03-07 17:22:30 +0000

    API-45 Gulp automated testing implemented

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon')
+var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
 
 gulp.task('default', function() {
     nodemon({
@@ -7,4 +8,10 @@ gulp.task('default', function() {
         ext: 'js html', 
         env: { 'NODE_ENV': 'development' }
       })
+});
+
+gulp.task('tests', function () {
+    return gulp.src(['tests/route-tests.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', util.log);
 });
