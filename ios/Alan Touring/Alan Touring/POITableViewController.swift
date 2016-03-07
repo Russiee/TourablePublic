d18298b93512b481a966e4d1353dc37816647d4b
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-03-07 22:25:27 +0000

    IOS-50 deleted unnecessary methods

--- ios/Alan Touring/Alan Touring/POITableViewController.swift
+++ ios/Alan Touring/Alan Touring/POITableViewController.swift
@@ -34,11 +34,6 @@ class POITableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    override func viewWillAppear(animated: Bool) {
-
-        self.tableView.reloadData()
-    }
-    
     func getPOIS(){
         POIList = []
         for POIS in (((NSUserDefaults.standardUserDefaults().objectForKey(superSectionID)) as! NSDictionary)["pois"]) as! NSArray{
@@ -226,12 +221,10 @@ class POITableViewController: UITableViewController {
             view.removeFromSuperview()
             
         }
-            cell.contentView.addSubview(poiViews[indexPath.row])
-            cell.selectionStyle = UITableViewCellSelectionStyle.None
-            cell.contentView.clipsToBounds = true
+        //adding the contents of the post into our tableView
+        cell.contentView.addSubview(poiViews[indexPath.row])
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
         
-        
-
         return cell
     }
     
@@ -240,40 +233,6 @@ class POITableViewController: UITableViewController {
     }
 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
