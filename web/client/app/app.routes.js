6319ca64404f89bf7f258c5291a38d971e8ab4dd
philefstat <phil.efstat@gmail.com>
2016-03-18 13:20:32 +0000

    WEB fix bug where website doesn't show up

--- web/client/app/app.routes.js
+++ web/client/app/app.routes.js
@@ -10,52 +10,52 @@ touring.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
     .state('home', {
         url: "/",
-        templateUrl: "/app/components/home/home.view.html",
+        templateUrl: "/client/app/components/home/home.view.html",
         controller: "HomeCtrl"
     })
     .state('tour', {
         url: "/tour?key",
-        templateUrl: "/app/components/tour/tour.view.html",
+        templateUrl: "/client/app/components/tour/tour.view.html",
         controller: "TourCtrl"
     })
 
 
     .state('admin', {
-        templateUrl: "/app/components/admin/admin.view.html",
+        templateUrl: "/client/app/components/admin/admin.view.html",
         controller: "AdminCtrl",
         authenticate: true
     })
     .state('admin.login', {
         url: "/admin/login",
         parent: 'admin',
-        templateUrl: "/app/components/admin/components/login.view.html",
+        templateUrl: "/client/app/components/admin/components/login.view.html",
         controller: "AdminCtrl"
     })
     .state('admin.dashboard', {
         url: "/admin",
         parent: 'admin',
-        templateUrl: "/app/components/admin/components/dashboard.view.html",
+        templateUrl: "/client/app/components/admin/components/dashboard.view.html",
         controller: "AdminCtrl",
         authenticate: true
     })
     .state('admin.manageTours', {
         url: "/admin/manage/tours",
         parent: 'admin',
-        templateUrl: "/app/components/admin/components/dashboard.view.html",
+        templateUrl: "/client/app/components/admin/components/dashboard.view.html",
         controller: "AdminCtrl",
         authenticate: true
     })
     .state('admin.manageAdmins', {
         url: "/admin/manage/admins",
         parent: 'admin',
-        templateUrl: "/app/components/admin/components/dashboard.view.html",
+        templateUrl: "/client/app/components/admin/components/dashboard.view.html",
         controller: "AdminCtrl",
         authenticate: true
     })
     .state('admin.account', {
         url: "/admin/account",
         parent: 'admin',
-        templateUrl: "/app/components/admin/components/dashboard.view.html",
+        templateUrl: "/client/app/components/admin/components/dashboard.view.html",
         controller: "AdminCtrl",
         authenticate: true
     });
