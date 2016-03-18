7e43320aec454be862fea001933553ebecf8feb5
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-03-18 16:24:27 +0000

    IOS-68 reduced print statements

--- ios/Tourable/Tourable/AddNewTourViewController.swift
+++ ios/Tourable/Tourable/AddNewTourViewController.swift
@@ -104,7 +104,6 @@ class addNewTourViewController: UIViewController, UIAlertViewDelegate {
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: { action in
             self.performSegueWithIdentifier("cancelAdd", sender: self)
-            print("this shit happens \(self.tourID)")
             TourDeleter.sharedInstance.deleteTour(self.tourID)
         }))
         alert.addAction(UIAlertAction(title: "Download with media (Larger)", style: UIAlertActionStyle.Default, handler: { action in
