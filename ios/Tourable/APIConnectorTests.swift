b5aa452bdb683fd76ca393ccca919bedb1347b51
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-03-18 14:23:03 +0000

    IOS-Testing-1 tests for api connector done checking connections with KCL-1010 and KCL-111 as they are our test tours. a test for connection and a performance test for each of those tours

--- ios/Tourable/APIConnectorTests.swift
+++ ios/Tourable/APIConnectorTests.swift
@@ -8,6 +8,11 @@
 
 import XCTest
 @testable import Tourable
+
+//tests just ot check that a connection is being established with the API, so only a connectivety 
+//tested here and not what happens with the data, as we arent testing that here
+//testing connection to our 2 test tours KCL-1010 and KCL-1111
+
 class APIConnectorTests: XCTestCase {
     
     override func setUp() {
@@ -27,12 +32,10 @@ class APIConnectorTests: XCTestCase {
         
     }
     
-    func testDownloadKCL1010() {
+    func testConnectionToAPIWithKCL1010() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let expectation = expectationWithDescription("connecting to api")
-        let ourData = NSMutableData()
-        var jsonResultFromAPI = NSDictionary()
+        let expectation = expectationWithDescription("connection to api established")
         let urlPath = "https://touring-api.herokuapp.com/api/v1/key/verify/KCL-1010"
         let request = NSURLRequest(URL: NSURL(string: urlPath)!)
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
@@ -41,18 +44,7 @@ class APIConnectorTests: XCTestCase {
         let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
             //XCTAssertNotNil(ourData, "the data should be nil as we havent received anything yet")
             XCTAssertNil(error, "there shouldn't be any error")
-            
-            ourData.appendData(data!)
-            do {
-                jsonResultFromAPI = try NSJSONSerialization.JSONObjectWithData(ourData, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
-                    expectation.fulfill()
-                
-                
-            }
-            catch let err as NSError{
-                //Need to let user know if the tourID they entered was faulty here
-                print(err.description)
-            }
+            expectation.fulfill()
             
         }
         task.resume()
@@ -67,11 +59,10 @@ class APIConnectorTests: XCTestCase {
         
     }
     
-    func testPerformanceOfAPIConnection() {
+    
+    func testPerformanceOfAPIConnectionWithKCL1010() {
         self.measureBlock {
             let expectation = self.expectationWithDescription("connecting to api")
-            let ourData = NSMutableData()
-            var jsonResultFromAPI = NSDictionary()
             let urlPath = "https://touring-api.herokuapp.com/api/v1/key/verify/KCL-1010"
             let request = NSURLRequest(URL: NSURL(string: urlPath)!)
             let config = NSURLSessionConfiguration.defaultSessionConfiguration()
@@ -80,18 +71,7 @@ class APIConnectorTests: XCTestCase {
             let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
                 //XCTAssertNotNil(ourData, "the data should be nil as we havent received anything yet")
                 XCTAssertNil(error, "there shouldn't be any error")
-                
-                ourData.appendData(data!)
-                do {
-                    jsonResultFromAPI = try NSJSONSerialization.JSONObjectWithData(ourData, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
-                    expectation.fulfill()
-                    
-                    
-                }
-                catch let err as NSError{
-                    //Need to let user know if the tourID they entered was faulty here
-                    print(err.description)
-                }
+                expectation.fulfill()
                 
             }
             task.resume()
@@ -103,6 +83,55 @@ class APIConnectorTests: XCTestCase {
                 task.cancel()
             }
 
+        }
+    }
+    
+    func testConnectionToAPIWithKCL1111(){
+        let expectation = expectationWithDescription("connecting to api")
+        let urlPath = "https://touring-api.herokuapp.com/api/v1/key/verify/KCL-1111"
+        let request = NSURLRequest(URL: NSURL(string: urlPath)!)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            //XCTAssertNotNil(ourData, "the data should be nil as we havent received anything yet")
+            XCTAssertNil(error, "there shouldn't be any error")
+            expectation.fulfill()
+        }
+        task.resume()
+        
+        waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+            task.cancel()
+        }
+
+    }
+    
+    func testPerformanceOfAPIConnectionWithKCL1111() {
+        self.measureBlock {
+            let expectation = self.expectationWithDescription("connecting to api")
+            let urlPath = "https://touring-api.herokuapp.com/api/v1/key/verify/KCL-1111"
+            let request = NSURLRequest(URL: NSURL(string: urlPath)!)
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            let task = session.dataTaskWithRequest(request) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                //XCTAssertNotNil(ourData, "the data should be nil as we havent received anything yet")
+                XCTAssertNil(error, "there shouldn't be any error")
+                expectation.fulfill()
+                
+            }
+            task.resume()
+            
+            self.waitForExpectationsWithTimeout(task.originalRequest!.timeoutInterval) { error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+                task.cancel()
+            }
+            
         }
     }
     
