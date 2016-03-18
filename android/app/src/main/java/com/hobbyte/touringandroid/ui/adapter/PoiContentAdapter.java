45e03795fa5f978a0832a577e9947ac3b735e76b
Jonathan Burton <jd_burton@hotmail.co.uk>
2016-03-18 11:43:58 +0000

    AND-51 Started work on video caching, routing url through a proxy so we can get at the data

--- android/app/src/main/java/com/hobbyte/touringandroid/ui/adapter/PoiContentAdapter.java
+++ android/app/src/main/java/com/hobbyte/touringandroid/ui/adapter/PoiContentAdapter.java
@@ -4,45 +4,34 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
-import android.graphics.SurfaceTexture;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.ThumbnailUtils;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
-import android.util.DisplayMetrics;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
 import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Surface;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.danikula.videocache.HttpProxyCacheServer;
 import com.hobbyte.touringandroid.App;
-import com.hobbyte.touringandroid.tourdata.ListViewItem;
 import com.hobbyte.touringandroid.R;
+import com.hobbyte.touringandroid.io.VideoStreamSaver;
+import com.hobbyte.touringandroid.tourdata.ListViewItem;
 import com.hobbyte.touringandroid.tourdata.Quiz;
-import com.hobbyte.touringandroid.ui.activity.TourActivity;
 import com.hobbyte.touringandroid.ui.fragment.POIFragment;
 import com.hobbyte.touringandroid.ui.fragment.VideoFragment;
 import com.hobbyte.touringandroid.ui.util.ImageCache;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -51,25 +40,23 @@ import java.util.regex.Pattern;
  * An {@link ArrayAdapter} that parses a POI JSON file to create the elements contained therein. A
  * POI can have any combination of the following item types:
  * <ul>
- *     <li>Header text</li><li>Body text</li>
- *     <li>An image with description</li><li>A video with description</li>
+ * <li>Header text</li><li>Body text</li>
+ * <li>An image with description</li><li>A video with description</li>
  * </ul>
- * <p>
+ * <p/>
  * Much of the code relating to image loading and caching was inspired by the Android
  * <a href="https://developer.android.com/training/displaying-bitmaps/index.html">
  * training guides</a>.
  */
 public class PoiContentAdapter extends ArrayAdapter<ListViewItem> {
-    private static final String TAG = "PoiContentAdapter";
-
     public static final int HEADER = 0;
     public static final int BODY = 1;
     public static final int IMAGE = 2;
     public static final int VIDEO = 3;
     public static final int QUIZ = 4;
-
-    private static Pattern namePattern;
+    private static final String TAG = "PoiContentAdapter";
     private static final String FILE_NAME_PATTERN = "https?:\\/\\/[-\\w\\.\\/]*\\/(.+\\.(jpe?g|png|mp4))";
+    private static Pattern namePattern;
     private ListViewItem[] items;
     private Bitmap loadingBitmap;
 
@@ -89,152 +76,6 @@ public class PoiContentAdapter extends ArrayAdapter<ListViewItem> {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.RGB_565;
         loadingBitmap = BitmapFactory.decodeResource(App.context.getResources(), R.drawable.poi_image_placeholder);
-    }
-
-    /**
-     * Returns the number of different POI element types. (Needed or else things break).
-     */
-    @Override
-    public int getViewTypeCount() {
-        return 5;
-    }
-
-    @Override
-        public int getItemViewType(int position) {
-        return items[position].getType();
-    }
-
-    /**
-     * Inflates a certain view depending on the type of ListViewItem (header, body, image, or video).
-     *
-     * @param position Position of item in the ItemList
-     * @param view View
-     * @param parent ParentView
-     * @return the inflated View
-     */
-    @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        ListViewItem listViewItem = items[position];
-        int listViewItemType = getItemViewType(position);
-        String filename = null;
-
-        TextView contentView;
-
-        if (listViewItem.getUrl() != null) {
-            Matcher m = namePattern.matcher(listViewItem.getUrl());
-            if (m.matches()) {
-                filename = m.group(1);
-            }
-        }
-
-        if (view == null) {
-            if (listViewItemType == IMAGE) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_image, parent, false);
-            } else if(listViewItemType == VIDEO) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_video, parent, false);
-            } else if (listViewItemType == HEADER) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_header, parent, false);
-            } else if (listViewItemType == QUIZ) {
-                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_quiz, parent, false);
-            } else {
-                    view = LayoutInflater.from(getContext()).inflate(R.layout.poi_content, parent, false);
-                }
-            }
-        switch (listViewItemType) {
-            case IMAGE:
-                ImageView imageView = (ImageView) view.findViewById(R.id.poiContentImageView);
-                TextView textView = (TextView) view.findViewById(R.id.poiContentImageDesc);
-                textView.setText(listViewItem.getText());
-
-                if (filename != null && taskNotAlreadyRunning(imageView)) {
-                    loadImageFromDiskOrCache(filename, imageView);
-                }
-
-                return view;
-
-            case VIDEO:
-                filePath = getContext().getFilesDir() + "/" + String.format("%s/video/%s", keyID, filename);
-                File file = new File(filePath);
-                if(file.exists()) {
-                    Bitmap bMap = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.MINI_KIND);
-                    ImageView thumbnail = (ImageView) view.findViewById(R.id.poiContentVideoView);
-                    thumbnail.setImageBitmap(bMap);
-                    thumbnail.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Activity activity = ((App) getContext().getApplicationContext()).getCurrentActivity();
-                            FragmentManager manager = activity.getFragmentManager();
-                            FragmentTransaction transaction = manager.beginTransaction();
-                            VideoFragment video = VideoFragment.newInstance(filePath);
-                            transaction.replace(R.id.fragmentContainer, video);
-                            transaction.addToBackStack(null);
-                            ((AppCompatActivity) activity).getSupportActionBar().setTitle("VideoPlayer");
-                            transaction.commit();
-                        }
-                    });
-                    TextView videoDesc = (TextView) view.findViewById(R.id.poiContentVideoDesc);
-                    videoDesc.setText(listViewItem.getText());
-                    return view;
-                } else {
-                    view = LayoutInflater.from(getContext()).inflate(R.layout.poi_content, parent, false);
-                    contentView = (TextView) view.findViewById(R.id.poiContentTextView);
-                    contentView.setText("This contains a video, please download the Tour with Media to view it!");
-                    return view;
-                }
-            case HEADER:
-                // TODO
-                if (view.findViewById(R.id.poiHeaderTextView) == null) {
-                    view = LayoutInflater.from(getContext()).inflate(R.layout.poi_header, parent, false);
-                }
-                contentView = (TextView) view.findViewById(R.id.poiHeaderTextView);
-                contentView.setText(listViewItem.getText() + "\n");
-                if(listViewItem.getText().length() == 0) {
-                    return new View(getContext());
-                }
-                return view;
-            case BODY:
-                // TODO
-                contentView = (TextView) view.findViewById(R.id.poiContentTextView);
-                contentView.setText(listViewItem.getText() + "\n");
-                return view;
-            case QUIZ:
-                contentView = (TextView) view.findViewById(R.id.quizTitle);
-                contentView.setText("Quiz: " + listViewItem.getText() + "\n");
-                if(quiz == null) {
-                    quiz = new Quiz(listViewItem.getOption(), listViewItem.getSolution(), view);
-                }
-                return view;
-            default:
-                contentView = (TextView) view.findViewById(R.id.poiContentTextView);
-                contentView.setText("Something went wrong\n");
-                return view;
-        }
-    }
-
-    /**
-     * First checks that the requested image hasn't already been loaded and saved in the cache. If
-     * this is not the case, then start an {@link AsyncTask} to load the image, if one hasn't
-     * already been triggered.
-     *
-     * @param filename the name of the file as it appears on disk
-     * @param imageView the {@link ImageView} that the image will be loaded into
-     */
-    public void loadImageFromDiskOrCache(String filename, ImageView imageView) {
-        ImageCache cache = ImageCache.getInstance();
-        Bitmap cachedBitmap = cache.getBitmap(filename);
-
-        if (cachedBitmap != null) {
-            imageView.setImageBitmap(cachedBitmap);
-        } else if (taskNotAlreadyRunning(imageView)) {
-            final ImageLoadingTask task = new ImageLoadingTask(imageView);
-
-            // will display the placeholder image immediately, but will be overwritten when the
-            // ImageLoadingTask has finished executing
-            final ASyncDrawable aSyncDrawable = new ASyncDrawable(App.context.getResources(), loadingBitmap, task);
-            imageView.setImageDrawable(aSyncDrawable);
-
-            task.execute(filename, keyID);
-        }
     }
 
     /**
@@ -266,6 +107,181 @@ public class PoiContentAdapter extends ArrayAdapter<ListViewItem> {
         }
 
         return null;
+    }
+
+    /**
+     * Returns the number of different POI element types. (Needed or else things break).
+     */
+    @Override
+    public int getViewTypeCount() {
+        return 5;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return items[position].getType();
+    }
+
+    /**
+     * Inflates a certain view depending on the type of ListViewItem (header, body, image, or video).
+     *
+     * @param position Position of item in the ItemList
+     * @param view     View
+     * @param parent   ParentView
+     * @return the inflated View
+     */
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ListViewItem listViewItem = items[position];
+        int listViewItemType = getItemViewType(position);
+        String filename = null;
+
+        TextView contentView;
+
+        if (listViewItem.getUrl() != null) {
+            Matcher m = namePattern.matcher(listViewItem.getUrl());
+            if (m.matches()) {
+                filename = m.group(1);
+            }
+        }
+
+        if (view == null) {
+            if (listViewItemType == IMAGE) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_image, parent, false);
+            } else if (listViewItemType == VIDEO) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_video, parent, false);
+            } else if (listViewItemType == HEADER) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_header, parent, false);
+            } else if (listViewItemType == QUIZ) {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_quiz, parent, false);
+            } else {
+                view = LayoutInflater.from(getContext()).inflate(R.layout.poi_content, parent, false);
+            }
+        }
+        switch (listViewItemType) {
+            case IMAGE:
+                ImageView imageView = (ImageView) view.findViewById(R.id.poiContentImageView);
+                TextView textView = (TextView) view.findViewById(R.id.poiContentImageDesc);
+                textView.setText(listViewItem.getText());
+
+                if (filename != null && taskNotAlreadyRunning(imageView)) {
+                    loadImageFromDiskOrCache(filename, imageView);
+                }
+
+                return view;
+
+            case VIDEO:
+                filePath = getContext().getFilesDir() + "/" + String.format("%s/video/%s", keyID, filename);
+                File file = new File(filePath);
+                if (file.exists()) {
+                    Bitmap bMap = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.MINI_KIND);
+                    ImageView thumbnail = (ImageView) view.findViewById(R.id.poiContentVideoView);
+                    thumbnail.setImageBitmap(bMap);
+                    thumbnail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Activity activity = ((App) getContext().getApplicationContext()).getCurrentActivity();
+                            FragmentManager manager = activity.getFragmentManager();
+                            FragmentTransaction transaction = manager.beginTransaction();
+                            VideoFragment video = VideoFragment.newInstance(filePath);
+                            transaction.replace(R.id.fragmentContainer, video);
+                            transaction.addToBackStack(null);
+                            ((AppCompatActivity) activity).getSupportActionBar().setTitle("VideoPlayer");
+                            transaction.commit();
+                        }
+                    });
+                    TextView videoDesc = (TextView) view.findViewById(R.id.poiContentVideoDesc);
+                    videoDesc.setText(listViewItem.getText());
+                    return view;
+                } else {
+                    Log.d(TAG, "File does not exist, going into 'else'");
+
+                    filePath = listViewItem.getUrl();
+                    Log.d(TAG, filePath);
+                    HttpProxyCacheServer proxy = App.getProxy();
+                    final String proxyFilePath = proxy.getProxyUrl(filePath);
+                    Log.d(TAG, "Aout to register cacheListener");
+                    proxy.registerCacheListener(new VideoStreamSaver(), filePath);
+                    Log.d(TAG, "registered cache listener");
+                    ImageView thumbnail = (ImageView) view.findViewById(R.id.imagePlayIcon);
+//                    Bitmap bMap = ThumbnailUtils.createVideoThumbnail(filePath, MediaStore.Video.Thumbnails.MINI_KIND);
+//                    thumbnail.setImageBitmap(bMap);
+                    thumbnail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Activity activity = ((App) getContext().getApplicationContext()).getCurrentActivity();
+                            FragmentManager manager = activity.getFragmentManager();
+                            FragmentTransaction transaction = manager.beginTransaction();
+                            VideoFragment video = VideoFragment.newInstance(proxyFilePath);
+                            transaction.replace(R.id.fragmentContainer, video);
+                            transaction.addToBackStack(null);
+                            ((AppCompatActivity) activity).getSupportActionBar().setTitle("VideoPlayer");
+                            transaction.commit();
+                        }
+                    });
+                    TextView videoDesc = (TextView) view.findViewById(R.id.poiContentVideoDesc);
+                    videoDesc.setText(listViewItem.getText());
+
+
+//                    view = LayoutInflater.from(getContext()).inflate(R.layout.poi_content, parent, false);
+//                    contentView = (TextView) view.findViewById(R.id.poiContentTextView);
+//                    contentView.setText("This contains a video, please download the Tour with Media to view it!");
+                    return view;
+                }
+            case HEADER:
+                // TODO
+                if (view.findViewById(R.id.poiHeaderTextView) == null) {
+                    view = LayoutInflater.from(getContext()).inflate(R.layout.poi_header, parent, false);
+                }
+                contentView = (TextView) view.findViewById(R.id.poiHeaderTextView);
+                contentView.setText(listViewItem.getText() + "\n");
+                if (listViewItem.getText().length() == 0) {
+                    return new View(getContext());
+                }
+                return view;
+            case BODY:
+                // TODO
+                contentView = (TextView) view.findViewById(R.id.poiContentTextView);
+                contentView.setText(listViewItem.getText() + "\n");
+                return view;
+            case QUIZ:
+                contentView = (TextView) view.findViewById(R.id.quizTitle);
+                contentView.setText("Quiz: " + listViewItem.getText() + "\n");
+                if (quiz == null) {
+                    quiz = new Quiz(listViewItem.getOption(), listViewItem.getSolution(), view);
+                }
+                return view;
+            default:
+                contentView = (TextView) view.findViewById(R.id.poiContentTextView);
+                contentView.setText("Something went wrong\n");
+                return view;
+        }
+    }
+
+    /**
+     * First checks that the requested image hasn't already been loaded and saved in the cache. If
+     * this is not the case, then start an {@link AsyncTask} to load the image, if one hasn't
+     * already been triggered.
+     *
+     * @param filename  the name of the file as it appears on disk
+     * @param imageView the {@link ImageView} that the image will be loaded into
+     */
+    public void loadImageFromDiskOrCache(String filename, ImageView imageView) {
+        ImageCache cache = ImageCache.getInstance();
+        Bitmap cachedBitmap = cache.getBitmap(filename);
+
+        if (cachedBitmap != null) {
+            imageView.setImageBitmap(cachedBitmap);
+        } else if (taskNotAlreadyRunning(imageView)) {
+            final ImageLoadingTask task = new ImageLoadingTask(imageView);
+
+            // will display the placeholder image immediately, but will be overwritten when the
+            // ImageLoadingTask has finished executing
+            final ASyncDrawable aSyncDrawable = new ASyncDrawable(App.context.getResources(), loadingBitmap, task);
+            imageView.setImageDrawable(aSyncDrawable);
+
+            task.execute(filename, keyID);
+        }
     }
 
     /**
