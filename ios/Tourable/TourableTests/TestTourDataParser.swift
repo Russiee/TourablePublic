2c8e35a18f8b6e5452d7afae9bf785dc7ea9142c
ozliftoff <gusgub@hotmail.co.uk>
2016-03-18 12:31:54 +0000

    IOS-TESTING TourDataParser tests

--- ios/Tourable/TourableTests/TestTourDataParser.swift
+++ ios/Tourable/TourableTests/TestTourDataParser.swift
@@ -21,22 +21,37 @@ class TestTourDataParser: XCTestCase {
         super.tearDown()
     }
 
+<<<<<<< 99b4cd1882adc06875d7ca0a6707fc269fe14594
     //Test the download of tour by triggering download and confirming result contains the correct tour markers
     func testTopLevelTourCreation() {
        
+=======
+    func testTopLevelTourCreation() {
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+>>>>>>> IOS-TESTING TourDataParser tests
         let bundleDownload = bundleRouteConnector()
         bundleDownload.startConnection("cjWRKDygIZ")
         let data = bundleDownload.getJSONResult()
         let result = tourDataParser().createNewTour(data)
 
+<<<<<<< 99b4cd1882adc06875d7ca0a6707fc269fe14594
         //Check result has all asociated types that are required.
+=======
+        //Check result has all asocited types that are required.
+>>>>>>> IOS-TESTING TourDataParser tests
         XCTAssertNotNil(result)
         XCTAssertNotNil(result.title.rangeOfString("title"))
         XCTAssertNotNil(result.title.rangeOfString("type"))
         XCTAssertNotNil(result.title.rangeOfString("Content"))
         
     }
+<<<<<<< 99b4cd1882adc06875d7ca0a6707fc269fe14594
     //Test the persisting a tour to NSUserDefaults, by saving a sample tour and then reading it again to confirm
+=======
+    
+>>>>>>> IOS-TESTING TourDataParser tests
     func testTourSave(){
         let bundleDownload = bundleRouteConnector()
         bundleDownload.startConnection("cjWRKDygIZ")
@@ -50,7 +65,10 @@ class TestTourDataParser: XCTestCase {
 
     }
     
+<<<<<<< 99b4cd1882adc06875d7ca0a6707fc269fe14594
     //test performance of persisting a tour to NSUserDefaults
+=======
+>>>>>>> IOS-TESTING TourDataParser tests
     func testSavePerformance(){
         
         self.measureBlock {
@@ -61,7 +79,10 @@ class TestTourDataParser: XCTestCase {
         }
     }
     
+<<<<<<< 99b4cd1882adc06875d7ca0a6707fc269fe14594
     //Test performance of reading a tour from persistant storage
+=======
+>>>>>>> IOS-TESTING TourDataParser tests
     func testReadPerformance(){
         
         let bundleDownload = bundleRouteConnector()
