38e97544e6c93b82cad1290319a3447f1419a1f6
pete2269 <peter_barta_3@hotmail.com>
2016-03-20 12:47:15 +0000

    admin tests documentation

--- api/tests/admin-test.js
+++ api/tests/admin-test.js
@@ -3,22 +3,30 @@ var assert = require('assert');
 var request = require('supertest');
 var admin = require('../routes/admin.js');
 
-
+//Admin route testing module functions
+//Includes all functions necessary to test the admin route
+//Checks the POST, PUT, GET, and DELETE functions
+//Uses hardcoded checks for data to be expected based on test input
+//All functions are called in the route-test.js file
 
 var adminTest = {
-
+    
+    //POST function tests
+    //creates and adds admin object to API database with given values
     POST: function(pointerID, url, callback){
+        //takes pointerID to link admin object to given organization
         var admin  = {
         "organization": ""+pointerID,
         "username": "Tester Name",
         "email": "test mail",
         "isSuper": true
         };
+        //sends object to API
         request(url)
        .post('api/v1/admin/')
        .send(admin)
-
-        .end(function(err, res) {
+        //checks to ensure the created object adheres to format requirements
+       .end(function(err, res) {
               if (err) {
                 throw err;
               }
@@ -28,18 +36,22 @@ var adminTest = {
               res.body.should.have.property("isSuper");
               res.status.should.be.equal(201);
               objID = res.body.objectId;
+              //uses callback to ensure tests run synchronously (for the purpose of linking objects through pointers)
               callback(objID);
         });
     },
 
 
 
-
+    //GET function tests
+    //first get function test to check object was added correctly
     GET1: function(pointerID, url, callback){
+        //queries the url with given objectID
         request(url)
         .get('api/v1/admin/'+pointerID)
         .expect('Content-Type', /json/)
         .expect(200 || 304) //Status code
+        //expected response, test fails if response is not the expected value
         .end(function(err,res) {
             if (err) {
                 throw err;
@@ -52,9 +64,10 @@ var adminTest = {
         });
     },
 
-
-
+    //PUT function tests
+    //updates the object 
     PUT: function(pointID, pointerID, url, callback){
+        //updates the object with new given values
        var admin2  =  {
         "organization": {
           "__type": "Pointer",
@@ -68,6 +81,7 @@ var adminTest = {
         request(url)
         .put('api/v1/admin/'+pointerID)
         .send(admin2)
+        //ensures response is correct by checking against expected values
         .end(function(err, res) {
               if (err) {
                 throw err;
@@ -82,7 +96,8 @@ var adminTest = {
           });
     },
 
-
+    //GET function tests
+    //second GET test to check object values were correctly updated
     GET2: function(pointerID, url, callback){
 
         request(url)
@@ -103,7 +118,8 @@ var adminTest = {
 
 
 
-
+    //DELETE function tests
+    //Deletes the test object
     DELETE: function(pointerID, url, callback){
 
         request(url)
@@ -116,7 +132,9 @@ var adminTest = {
             callback();
         });
     },
-
+    
+    //GET function tests
+    //third GET test to check objet no longer exists / object was correctly deleted
     GET3: function(pointerID, url, callback){
         request(url)
         .get('api/v1/admin/'+pointerID)
