2a47ce628277cd409c1b17b3fccb3dffbd38ce66
philefstat <phil.efstat@gmail.com>
2016-03-21 14:27:13 +0000

    WEB admin add put model for admins

--- web/client/app/components/admin/admin.model.js
+++ web/client/app/components/admin/admin.model.js
@@ -35,6 +35,25 @@ tourable.factory('adminFactory', function($http, $q) {
                     //or server returns response with an error status.
                 });
             });
+        },
+        putAdmin: function(data) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'PUT',
+                    url: 'https://api.tourable.org/api/v1/admin/' + id,
+                    data: data
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
         }
     };
 });
