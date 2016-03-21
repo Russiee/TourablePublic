b9b93f3f2d6eada4c60efbc9bd972ff3ae3145cd
philefstat <phil.efstat@gmail.com>
2016-03-21 17:25:29 +0000

    WEB admin fix tour lists on manage tours

--- web/client/app/components/admin/admin.controller.js
+++ web/client/app/components/admin/admin.controller.js
@@ -22,7 +22,6 @@ angular.module('tourable')
                 if ($scope.admin.isSuper) {
                     getOrganization(response.data.organization.objectId);
                     getAllAdmins(response.data.organization.objectId);
-                    getAllTours(response.data.organization.objectId);
                 }
             }, function(error) {
                 //Console log in case we need to debug with a user
@@ -49,17 +48,37 @@ angular.module('tourable')
             getAllAdminData.then(function(response) {
                 console.log('Success: ', response.data);
                 $scope.admins = response.data;
+                getAllTours();
             }, function(error) {
                 //Console log in case we need to debug with a user
                 console.log('An error occured while retrieving the all-admin data: ', error);
             });
         }
 
-        function getAllTours (orgID) {
-            var getAllTourData = adminFactory.getAllTours(orgID);
+        function getAllTours () {
+            var getAllTourData = adminFactory.getAllTours();
             getAllTourData.then(function(response) {
                 console.log('Success: ', response.data);
-                $scope.tours = response.data;
+                $scope.tours = {
+                    thisAdmin: [],
+                    otherAdmins: []
+                };
+                for (var index in response.data) {
+                    var tour = response.data[index];
+
+                    if (tour.admin && tour.admin.objectId === $scope.admin.objectId) {
+                        console.log("Compare " + tour.admin.objectId + " to " + $scope.admin.objectId);
+                        $scope.tours.thisAdmin.push(tour);
+                    } else if (tour.admin) {
+                        for (var _index in $scope.admins) {
+                            console.log("Compare " + tour.admin.objectId + " to " + $scope.admins[_index].objectId);
+                            if (tour.admin.objectId === $scope.admins[_index].objectId) {
+                                $scope.tours.otherAdmins.push(tour);
+                            }
+                        }
+                    }
+                }
+                console.log("tours", $scope.tours);
             }, function(error) {
                 //Console log in case we need to debug with a user
                 console.log('An error occured while retrieving all tours: ', error);
