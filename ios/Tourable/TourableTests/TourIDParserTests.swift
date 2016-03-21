b792b75545c3dff9cd3c46a5c6e38cfc25fcbbc0
Soviet <daniil.baryshnikov@kcl.ac.uk>
2016-03-21 13:15:39 +0000

    IOS-Testing TourIDParser Tests are done

--- ios/Tourable/TourableTests/TourIDParserTests.swift
+++ ios/Tourable/TourableTests/TourIDParserTests.swift
@@ -52,40 +52,4 @@ class TourIDParserTests: XCTestCase {
         
     }
     
-    func testGettingTourMetaData(){
-        
-        let metaData = [
-            "tour": [
-                "__type": "Pointer",
-                "className": "Tour",
-                "objectId": "cjWRKDygIZ"
-            ],
-            "code": "KCL-1010",
-            "updatedAt": "2016-03-20T12:10:42.175Z",
-            "createdAt": "2016-03-18T10:50:47.172Z",
-            "expiry": "2016-06-19T00:00:00.000Z",
-            "objectId": "ZX8DHpGKxk"
-        ] 
-        
-        let tourDict = metaData["tour"] as! NSMutableDictionary
-        tourDict["expiresAt"] = metaData["expiresAt"]
-        tourDict["updatedAt"] = metaData["updatedAt"]
-        tourDict["createdAt"] = metaData["createdAt"]
-        
-        NSUserDefaults.standardUserDefaults().setObject(tourDict, forKey: "KCL-1010")
-        NSUserDefaults.standardUserDefaults().synchronize()
-        
-        XCTAssertEqual(tourDict, TourIdParser.sharedInstance.getTourMetadata("KCL-1010"), "Should be identical tour metaData")
-        
-    }
-    
-    
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
