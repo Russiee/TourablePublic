be22aed6047e0c59a5cda624c151418760c3179c
philefstat <phil.efstat@gmail.com>
2016-03-21 16:48:58 +0000

    WEB admin fix manage tours styling and add get all tours model

--- web/client/app/components/admin/admin.model.js
+++ web/client/app/components/admin/admin.model.js
@@ -72,6 +72,24 @@ tourable.factory('adminFactory', function($http, $q) {
                     //or server returns response with an error status.
                 });
             });
-        }
+        },
+        getAllTours: function(orgID) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/tours?organization=' + orgID
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        },
     };
 });
