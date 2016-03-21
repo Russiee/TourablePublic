bb08f8fce0329c1dec7c28520a047ba7c4bc8714
philefstat <phil.efstat@gmail.com>
2016-03-21 12:02:17 +0000

    WEB fix login authservice, remove unnecssary parent param from routes, implement models in admin controller

--- web/client/app/components/admin/admin.controller.js
+++ web/client/app/components/admin/admin.controller.js
@@ -1,15 +1,51 @@
 angular.module('tourable')
-    .controller('AdminCtrl', function ($scope, AuthService, $state, $location) {
+    .controller('AdminCtrl', function ($scope, AuthService, $state, $location, adminFactory) {
 
         $scope.$on('loginStatusChanged', function (event, isLoggedIn) {
+            console.log("hello");
             if (isLoggedIn){
                 console.log("hello");
-                $state.transitionTo("admin.dashboard");
+                $state.go("admin.dashboard");
+            } else {
+                $state.go("home");
             }
         });
 
+        if (AuthService.isLoggedIn()) {
+            var getAdminData = adminFactory.getAdmin(AuthService.currentUser().id);
+            getAdminData.then(function(response) {
+                console.log('Success: ', response.data);
+                $scope.admin = response.data;
+                getOrganization(response.data.organization.objectId);
+            }, function(error) {
+                //Console log in case we need to debug with a user
+                console.log('An error occured while retrieving the admin data: ', error);
+                //Redirect back to homepage
+                $location.url('/');
+            });
+
+        }
+
+        function getOrganization (id) {
+            var getOrganizationData = adminFactory.getOrganization(id);
+            getOrganizationData.then(function(response) {
+                console.log('Success: ', response.data);
+                $scope.organization = response.data;
+            }, function(error) {
+                //Console log in case we need to debug with a user
+                console.log('An error occured while retrieving the admin data: ', error);
+            });
+        }
+
+
+
         $scope.login = function () {
-            AuthService.login($scope.email, $scope.password);
+            //if already logged in
+            if (AuthService.isLoggedIn()) {
+                $state.go("admin.dashboard");
+            } else {
+                AuthService.login($scope.login.email, $scope.login.password);
+            }
         };
 
         $scope.logout = function () {
--- web/client/app/components/admin/admin.model.js
+++ web/client/app/components/admin/admin.model.js
@@ -17,6 +17,24 @@ tourable.factory('adminFactory', function($http, $q) {
                     //or server returns response with an error status.
                 });
             });
+        },
+        getAdmin: function(id) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/admin/' + id
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
         }
     };
 });
