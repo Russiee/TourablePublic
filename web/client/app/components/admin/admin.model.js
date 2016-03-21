9b95f343f0116f604f385edb5472c20fe9f23265
philefstat <phil.efstat@gmail.com>
2016-03-21 11:47:13 +0000

    WEB admin implement organization model

---
+++ web/client/app/components/admin/admin.model.js
@@ -0,0 +1,22 @@
+tourable.factory('adminFactory', function($http, $q) {
+    return {
+        getOrganization: function(id) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/organization/' + id
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        }
+    };
+});
