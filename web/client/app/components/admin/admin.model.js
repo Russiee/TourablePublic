88be9ae9997f6bda690ba869a964aaadf67fb763
philefstat <phil.efstat@gmail.com>
2016-03-21 15:30:08 +0000

    WEB admin temp commit, build manage admin markup

--- web/client/app/components/admin/admin.model.js
+++ web/client/app/components/admin/admin.model.js
@@ -36,6 +36,24 @@ tourable.factory('adminFactory', function($http, $q) {
                 });
             });
         },
+        getAllAdmins: function(orgID) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/admin/' + id
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        },
         putAdmin: function(data) {
             return $q(function(resolve, reject) {
                 $http({
