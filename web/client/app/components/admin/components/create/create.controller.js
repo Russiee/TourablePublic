e436c63e3cd5202e8f050fc7bc0169430b233c67
philefstat <phil.efstat@gmail.com>
2016-03-21 18:36:23 +0000

    WEB admin lay framework for create controller

--- web/client/app/components/admin/components/create/create.controller.js
+++ web/client/app/components/admin/components/create/create.controller.js
@@ -1,4 +1,37 @@
 angular.module('tourable')
-    .controller('TourCtrl', function ($scope, $location, $state) {
+    .controller('CreateCtrl', function ($scope, $location, $state) {
+        String.prototype.capitalize = function() {
+            return this.charAt(0).toUpperCase() + this.slice(1);
+        }
+
         $scope.class = $state.params.class;
+
+        $scope.classData = {
+            tour: {
+                expectedInput: [
+                    {
+                        description: "Tour Title",
+                        model: "title",
+                        type: "text",
+                        help: ""
+                    },
+                    {
+                        description: "Tour Description",
+                        model: "description",
+                        type: "textarea-small",
+                        help: ""
+                    }
+                ],
+                afterCreate: "admin.manageTours"
+            }
+        }
+
+        $scope.create = function() {
+
+        }
+
+        //redirect back to dashboard if class is null or does not exist
+        if (!$state.params.class || !$scope.classData[$state.params.class]) {
+            $state.go('admin.dashboard');
+        }
     });
