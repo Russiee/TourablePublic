1d48a1fd1060b7872d4c57125013f91701904780
Jonathan Burton <jd_burton@hotmail.co.uk>
2016-03-22 14:32:36 +0000

    AND-71 All ServerApi tests pass

--- android/app/src/androidTest/java/com/hobbyte/touringandroid/internet/ServerAPITest.java
+++ android/app/src/androidTest/java/com/hobbyte/touringandroid/internet/ServerAPITest.java
@@ -2,12 +2,15 @@ package com.hobbyte.touringandroid.internet;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Jonathan
@@ -54,11 +57,17 @@ public class ServerAPITest {
         String testKey = "not-a-key-test";
         JSONObject keyJSON;
 
-        //use valid key
+        //performs call
         keyJSON = ServerAPI.checkKeyValidity(testKey);
-        assertEquals(null, keyJSON);
+
+        //check
+        assertEquals(keyJSON, null);
     }
 
+    /**
+     * Checks that the ServerAPI call to CheckKeyValidity returns null for an expired key
+     * @throws Exception
+     */
     @Test
     public void checkKeyValidity_expired() throws Exception {
 
@@ -66,11 +75,86 @@ public class ServerAPITest {
         String testKey = "KCL-TEST-EXPIRED";
         JSONObject keyJSON;
 
-        //use valid key
+        //perform call
         keyJSON = ServerAPI.checkKeyValidity(testKey);
-        assertEquals(null, keyJSON);
+
+        //check
+        assertEquals(keyJSON, null);
     }
 
+
+    /**
+     * Check that the ServerAPI call gets the correct value
+     * @throws Exception
+     */
+    @Test
+    public void checkGetJSON_valid_bundle() throws Exception {
+
+        //value to test
+        String id = "cjWRKDygIZ";
+
+        //perform call
+        JSONObject bundleJSON = ServerAPI.getJSON(id, ServerAPI.BUNDLE);
+
+        //we should get a response back
+        assert bundleJSON != null;
+
+        //bundle is the only response to contain this array
+        JSONArray sections = bundleJSON.getJSONArray("sections");
+        assert sections != null;
+
+        String sectionsString = sections.toString();
+        //only poi objects have post and therefore only the bundle
+        //there is the possibility for a tour to simply have no poi's
+        //but that is a case that the cms should enforce
+        assertTrue(sectionsString.contains("post"));
+
+        //both the bundle and the tour contain the description tag, but the keyJSON doesn't
+        assert bundleJSON.getString("description") != null;
+
+    }
+
+    /**
+     * Check that the ServerAPI call gets the correct value
+     * @throws Exception
+     */
+    @Test
+    public void checkGetJSON_valid_tour() throws Exception {
+
+        //value to test
+        String id = "cjWRKDygIZ";
+
+        //perform action
+        JSONObject tourJSON = ServerAPI.getJSON(id, ServerAPI.TOUR);
+
+        //should get a result
+        assert tourJSON != null;
+
+        //only poi objects have this array and therefore only the bundle
+        //therefore we assertNull
+        assertFalse(tourJSON.has("sections"));
+
+        //both the bundle and the tour contain the description tag, but the keyJSON doesn't
+        assert tourJSON.getString("description") != null;
+
+    }
+
+    /**
+     * Check that the ServerAPI call fails cleanly
+     * @throws Exception
+     */
+    @Test
+    public void checkGetJSON_invalid() throws Exception {
+
+        //value to test
+        String id = "not-an-id";
+
+        //perform action
+        JSONObject tourJSON = ServerAPI.getJSON(id, ServerAPI.TOUR);
+
+        //should get null back
+        assertEquals(tourJSON, null);
+    }
 
 
 
