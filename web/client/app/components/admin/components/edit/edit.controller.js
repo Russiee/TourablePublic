db6c1181b1abb79921441de88147077a6b3400c0
philefstat <phil.efstat@gmail.com>
2016-03-22 04:52:33 +0000

    WEB admin implement section page logic

--- web/client/app/components/admin/components/edit/edit.controller.js
+++ web/client/app/components/admin/components/edit/edit.controller.js
@@ -1,9 +1,5 @@
 angular.module('tourable')
-    .controller('EditCtrl', function ($scope, $location, $state, classDataFactory, editFactory) {
-
-        String.prototype.capitalize = function() {
-            return this.charAt(0).toUpperCase() + this.slice(1);
-        }
+    .controller('EditCtrl', function ($rootScope, $scope, $state, editFactory) {
 
         for (var index in $scope.$parent.tours.all) {
             if ($scope.$parent.tours.all[index].objectId === $state.params.id) {
@@ -11,29 +7,66 @@ angular.module('tourable')
             }
         }
 
-        console.log($scope.tour);
+        if (!$state.params.id) {
+            $state.go('admin.manageTours');
+        }
 
-        var getTour = editFactory.getTour($state.params.id);
-        getTour.then(function(response) {
-            console.log('Success: ', response.data);
-            var keys = $scope.tour.keys;
-            $scope.tour = response.data;
-            $scope.tour.keys = keys;
-            sessionStorage.setItem('tour', JSON.stringify($scope.tour));
-        }, function(error) {
-            //Console log in case we need to debug with a user
-            console.log('An error occured while retrieving the admin data: ', error);
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            if (!toParams.id) {
+                $state.go('admin.manageTours');
+            }
+            if (toState.name === 'admin.edit.tour') {
+                getTourData();
+                getKeyData();
+            } else if (toState.name === 'admin.edit.section') {
+                 getSectionData();
+            }
         });
 
-        var getKeys = editFactory.getKeys($state.params.id);
-        getKeys.then(function(response) {
-            console.log('Keys: ', response.data);
-            $scope.tour.keys = response.data;
-            sessionStorage.setItem('tour.keys', JSON.stringify($scope.tour.keys));
-        }, function(error) {
-            //Console log in case we need to debug with a user
-            console.log('An error occured while retrieving the admin data: ', error);
-        });
+        getTourData();
+        getKeyData();
+
+        if ($state.current.name === 'admin.edit.section') {
+            getSectionData();
+        }
+
+        function getTourData() {
+            var getTour = editFactory.getTour($state.params.id);
+            getTour.then(function(response) {
+                console.log('Success: ', response.data);
+                var keys = $scope.tour.keys;
+                $scope.tour = response.data;
+                $scope.tour.keys = keys;
+                sessionStorage.setItem('tour', JSON.stringify($scope.tour));
+            }, function(error) {
+                //Console log in case we need to debug with a user
+                console.log('An error occured while retrieving the admin data: ', error);
+            });
+        }
+
+        function getKeyData() {
+            var getKeys = editFactory.getKeys($state.params.id);
+            getKeys.then(function(response) {
+                console.log('Keys: ', response.data);
+                $scope.tour.keys = response.data;
+                sessionStorage.setItem('tour.keys', JSON.stringify($scope.tour.keys));
+            }, function(error) {
+                //Console log in case we need to debug with a user
+                console.log('An error occured while retrieving the admin data: ', error);
+            });
+        }
+
+        function getSectionData () {
+            var getSection = editFactory.getSection($state.params.id);
+            getSection.then(function(response) {
+                console.log('Section: ', response.data);
+                $scope.section = response.data;
+                sessionStorage.setItem('section', JSON.stringify($scope.tour.keys));
+            }, function(error) {
+                //Console log in case we need to debug with a user
+                console.log('An error occured while retrieving the admin data: ', error);
+            });
+        }
 
         $scope.createTopSection = function() {
             $state.go('admin.create', {
