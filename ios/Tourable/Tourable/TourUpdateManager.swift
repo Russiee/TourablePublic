6270471af9b1e40026d4aadf9ef6b320dcb8213c
FedericoKCL <fed.deployment@aol.co.uk>
2016-03-22 18:15:26 +0000

    IOS-74 removed unused methods inside TourUpdateManager

--- ios/Tourable/Tourable/TourUpdateManager.swift
+++ ios/Tourable/Tourable/TourUpdateManager.swift
@@ -193,15 +193,6 @@ public class TourUpdateManager: NSObject {
 // credit to: Leo Dabus
 // http://stackoverflow.com/questions/27182023/getting-the-difference-between-two-nsdates-in-months-days-hours-minutes-seconds
 extension NSDate {
-    func yearsFrom(date:NSDate) -> Int{
-        return NSCalendar.currentCalendar().components(.Year, fromDate: date, toDate: self, options: []).year
-    }
-    func monthsFrom(date:NSDate) -> Int{
-        return NSCalendar.currentCalendar().components(.Month, fromDate: date, toDate: self, options: []).month
-    }
-    func weeksFrom(date:NSDate) -> Int{
-        return NSCalendar.currentCalendar().components(.WeekOfYear, fromDate: date, toDate: self, options: []).weekOfYear
-    }
     func daysFrom(date:NSDate) -> Int{
         return NSCalendar.currentCalendar().components(.Day, fromDate: date, toDate: self, options: []).day
     }
@@ -211,18 +202,5 @@ extension NSDate {
     func minutesFrom(date:NSDate) -> Int{
         return NSCalendar.currentCalendar().components(.Minute, fromDate: date, toDate: self, options: []).minute
     }
-    func secondsFrom(date:NSDate) -> Int{
-        return NSCalendar.currentCalendar().components(.Second, fromDate: date, toDate: self, options: []).second
-    }
-    
-    func offsetFrom(date:NSDate) -> String {
-        if yearsFrom(date)   > 0 { return "\(yearsFrom(date))y"   }
-        if monthsFrom(date)  > 0 { return "\(monthsFrom(date))M"  }
-        if weeksFrom(date)   > 0 { return "\(weeksFrom(date))w"   }
-        if daysFrom(date)    > 0 { return "\(daysFrom(date))d"    }
-        if hoursFrom(date)   > 0 { return "\(hoursFrom(date))h"   }
-        if minutesFrom(date) > 0 { return "\(minutesFrom(date))m" }
-        if secondsFrom(date) > 0 { return "\(secondsFrom(date))s" }
-        return ""
-    }
+
 }
