b512fa89edbf439c7677e0ebd5124c82bb98c35b
philefstat <phil.efstat@gmail.com>
2016-03-22 04:36:05 +0000

    WEB admin implement get section route

--- web/client/app/components/admin/components/edit/edit.model.js
+++ web/client/app/components/admin/components/edit/edit.model.js
@@ -37,6 +37,24 @@ tourable.factory('editFactory', function($http, $q) {
                 });
             });
         },
+        getSection: function(id) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/section/' + id
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        },
         getKeys: function(tourID) {
             return $q(function(resolve, reject) {
                 $http({
