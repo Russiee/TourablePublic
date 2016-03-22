59f9c547b8c9bba5087ccdc9fbcc19c4e101cef7
philefstat <phil.efstat@gmail.com>
2016-03-22 01:58:22 +0000

    WEB admin add get tour model

---
+++ web/client/app/components/admin/components/edit/edit.model.js
@@ -0,0 +1,41 @@
+tourable.factory('editFactory', function($http, $q) {
+    return {
+        edit: function(className, data) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'PUT',
+                    url: 'https://api.tourable.org/api/v1/' + className,
+                    data: data
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        },
+        getTour: function(id) {
+            return $q(function(resolve, reject) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tourable.org/api/v1/tour/' + id
+                }).then(function success(response) {
+                    console.log(response);
+                    resolve (response);
+                    //this callback will be called asynchronously
+                    //when the response is available
+                }, function error(response) {
+                    console.log("error", response);
+                    reject (response);
+                    //called asynchronously if an error occurs
+                    //or server returns response with an error status.
+                });
+            });
+        },
+    };
+});
